41b756089f6cf1cd31637253e9dbf49b78246406
sroskom <sterlingroskom@gmail.com>
2017-08-07 03:39:16 -0500

    change home url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='topwidget',
       version='0.1',
       description='useful top widget for kivy',
-      url='https://github.com/sroskom/test_projects',
+      url='https://github.com/sroskom/packages/tree/topwidget',
       author='STROS',
       author_email='sterlingroskom@gmail.com',
       license='Copyright Protected',
